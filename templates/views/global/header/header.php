0a899f772032f1ad9529c408d338969dd0423dfd
Rafael Peralta Jr <rperaltajr@gmail.com>
2021-01-22 11:48:32 -0500

    [header] update page title

--- templates/views/global/header/header.php
+++ templates/views/global/header/header.php
@@ -5,7 +5,13 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
-    <title><?php echo get_bloginfo( 'name' ) . " - " ?? ''; ?><?php the_title(); ?></title>
+    <?php 
+        global $post;
+        if(get_the_title($post) != get_the_title($post->post_parent)): 
+            $parent_page = get_the_title( $post->post_parent );
+        endif;
+    ?>
+    <title><?php echo get_bloginfo( 'name' ) . " - " ?? ''; ?><?php if(!empty($parent_page)): echo $parent_page . ' - '; endif; the_title(); ?></title>
 
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/build/css/<?php echo $data->type; ?>.css?ver=<?php echo rand(); ?>">
 
